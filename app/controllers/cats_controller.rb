212a2ccd566f0cfa91cc5ea1d9956dc5c8534f86
Chris Bigelow <chrisbigelow25@gmail.com>
2017-10-24 15:16:59 -0700

    completed phase 1

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,8 +6,48 @@ class CatsController < ApplicationController
   end
 
   def show
-    @cat = Cat.find(params[:id])
+    @cat = Cat.find_by(id: params[:id])
+    if @cat
+      render :show
+    else
+      redirect_to cats_url
+    end
+  end
 
-    render :show
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cats_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :description, :birth_date)
   end
 end
